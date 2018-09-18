689cf4eee85e582c882b56429e365f034ad401aa
pavan kumar <mvnpavan@gmail.com>
2018-09-18 15:35:16 +0530

    Initial Commit

---
+++ DataMonitor.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using DataMonitor.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataMonitor.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options) {}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
