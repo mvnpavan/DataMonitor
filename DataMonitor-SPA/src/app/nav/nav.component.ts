fe6e82b4da4ce9bc1ea8b64ad87b3dc0507da119
pavan kumar <mvnpavan@gmail.com>
2018-09-20 12:54:49 +0530

    Adding global error handling

--- DataMonitor-SPA/src/app/nav/nav.component.ts
+++ DataMonitor-SPA/src/app/nav/nav.component.ts
@@ -20,7 +20,7 @@ export class NavComponent implements OnInit {
       console.log('Logged in Successfully');
       this.loggedIn();
     }, error => {
-      console.log('Failed to login');
+      console.log(error);
     });
   }
 
